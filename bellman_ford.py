67d644f5155a1a6688dc8fc6fe309027177892fb
RyuJinHyeok <101165696+RyuJinHyeok@users.noreply.github.com>
2023-05-19 03:20:37 +0900

    벨만포드 구현 완료

---
+++ bellman_ford.py
@@ -0,0 +1,57 @@
+import json
+
+# set INF value
+INF = 9999999
+
+# bellman-ford algorithm
+def bellman_ford(graph, start, end):
+    # init
+    dist = {(station, line) : INF if station != start else 0 for station in graph.keys() for line in graph[station].keys()}
+    parent = {(station, line) : None for station in graph.keys() for line in graph[station].keys()}
+
+    # caculate distance
+    n = len(dist)
+    for _ in range(n - 1):
+        for parent_station in dist.keys():
+            for station, line, cost in graph[parent_station[0]][parent_station[1]]:
+                child_station = (station, line)
+                if dist[child_station] > dist[parent_station] + cost:
+                    dist[child_station]  = dist[parent_station] + cost
+
+
+    min_cost = INF
+    min_line = None
+    for line in graph[end].keys():
+        if dist[(end, line)] < min_cost:
+            min_cost = dist[(end, line)]
+            min_line = line
+            
+    
+    node = (end, min_line)
+    path = []
+    while node[0] != start:
+        path.append(node)
+        node = parent[node]
+    path.append(node)
+    path.reverse()
+
+    return min_cost, path
+
+with open('subway(real).json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+start = input('출발지: ')
+end = input('도착지: ')
+
+result = bellman_ford(data, start, end)
+
+print('소요시간:', result[0])
+
+print(start, end='')
+cur_line = result[1][0][1]
+for index, data in enumerate(result[1]):
+    if data[0] == start: continue
+    print('(환승)' if cur_line != data[1] else '-%s' % data[0], end='')
+    cur_line = data[1]
+
+print()
